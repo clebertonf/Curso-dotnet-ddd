2edd9dbe71a7d8cd1f94e2179130d3bf6a33816c
Cleberton <clebertonfgc@gmail.com>
2021-10-11 22:36:39 -0300

    Add dbContext

---
+++ Api/Data/Context/MyContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Context
+{
+    class MyContext : DbContext
+    {
+        public DbSet<UserEntity> Users { get; set; }
+    }
+}
