0e5a516f5f5c8681018a6ca03816622bb7c58094
Cleberton <clebertonfgc@gmail.com>
2021-10-11 21:40:50 -0300

    Base entity e user entity

---
+++ Api/Domain/Entities/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    class BaseEntity
+    {
+        public Guid ID { get; set; }
+
+        private DateTime? _createAt;
+
+        public DateTime? CreateAt
+        {
+            get { return _createAt; }
+            set { _createAt = _createAt == null ? DateTime.UtcNow : value; }
+        }
+
+        public DateTime UpdateAt { get; set; }
+    }
+}
---
+++ Api/Domain/Entities/UserEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    class UserEntity : BaseEntity
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+    }
+}
